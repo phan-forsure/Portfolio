f92a5159f89634fd97e0ddbb7ff27f8135490891
phan-forsure <phantommy38@gmail.com>
2024-07-17 08:02:12 +0300

    updated

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-
 export default function Hero() {
   return (
-    <section className="hero m-12">
+    <section className="hero m-12 flex flex-wrap content-between">
         <div className='text-4xl font-extralight'>
             <h1 className='mb-8 font-normal'>Hello!</h1>
             <p className='mb-4'>I'm a <span className='font-normal'>Abdulrahman</span>, a passionate front-end web developer who builds unique experiences & interfaces.</p>
+            <p className='text-2xl mt-12'>Get in touch <a className='border-b-2 border-b-main transition-all hover:bg-blue-800' href='mailto:phantommy38@gmail.com'>phantommy38@gmail.com</a></p>
         </div>
-        <p className='text-2xl mt-12'>Get in touch <a className='border-b-2 border-b-main transition-all hover:bg-blue-800' href='mailto:phantommy38@gmail.com'>phantommy38@gmail.com</a></p>
+        <div className="w-full m-2 flex">
+          <a href="https://github.com/phan-forsure" className="block w-fit mx-2"><img className="w-8 h-8 rounded-xl opacity-50 hover:opacity-85 transition-all cursor-pointer" src="src/assets/githubicon-removebg-preview.png" alt="github" /></a>
+          <a href="https://codepen.io/phan-forsure" className="block w-fit mx-2"><img className="w-8 h-8 rounded-xl opacity-50 hover:opacity-85 transition-all cursor-pointer" src="src/assets/codepen1600.png" alt="codepen" /></a>
+        </div>
     </section>
   )
 }
--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 const languages: string[] = ['HTML 5', 'CSS3 & Sass', 'JavaScript + ES6', 'TypeScript']
 const frameworks: string[] = ['React', 'Tailwind CSS', 'React Query', 'Axios']
 const tools: string[] = ['Vite', 'Webpack', 'Postman', 'Bash', 'Git & Github', 'Chrome DevTool', 'ESLint']
 
 function Skills() {
     return (
-        <div className='skills rounded-md hover:bg-main-light transition-all p-8 mt-4'>
+        <div className='comp'>
             <h2 className='text-xl font-semibold text-center border-b-1 border-b-main mx-6 mb-8 p-4'>Skills</h2>
             <div className='grid grid-cols-3'>
                 <ul className='flex flex-wrap justify-center border-r-1 border-r-main'>
@@ -26,11 +24,37 @@ function Skills() {
     )
 }
 
+function Experience() {
+    return (
+        <div className='comp'>
+            <h2 className='text-xl font-semibold text-center border-b-1 border-b-main mx-6 p-4'>Experience</h2>
+            <div className='grid grid-rows-3'>
+               <h2 className="p-8 text-xl mx-6 border-b-1 border-b-main opacity-70">10+ Projects & Practices</h2>
+               <h2 className="p-8 text-xl mx-6 border-b-1 border-b-main opacity-70">10+ Github Repositories</h2>
+               <h2 className="p-8 text-xl mx-6 opacity-70">5 Deployed Websites on Netlify</h2>
+            </div>
+        </div>
+    )
+}
+
+function Projects() {
+    return (
+        <div className='comp'>
+            <h2 className='text-xl font-semibold text-center border-b-1 border-b-main mx-6 p-4'>Projects</h2>
+            <div className='project'>
+                <h2 className="p-8 mx-6 opacity-70">E-commerce App - Website</h2>
+            </div>
+        </div>
+    )
+}
+
 export default function Sidebar() {
   return (
     <section className="side-bar px-20 overflow-y-scroll">
-        <p className='opacity-70 px-8 py-4 text-center'>I have the required skills to make good looking interfaces yet functional & performant web applications.</p>
+        <p className='opacity-70 px-8 py-4 text-center'>I have the required skills to make pixel-perfect interfaces yet functional & performant web applications.</p>
         <Skills />
+        <Experience />
+        <Projects />
     </section>
   )
 }
