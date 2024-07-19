1a2d4858996e257a6656e72b2ecc74887fc77ae7
phan-forsure <phantommy38@gmail.com>
2024-07-19 20:17:55 +0300

    updated

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -5,10 +5,11 @@ export default function Hero() {
             <h1 className='mb-8 font-normal'>Hello!</h1>
             <p className='mb-4'>I'm a <span className='font-normal'>Abdulrahman</span>, a passionate front-end web developer who builds unique experiences & interfaces.</p>
             <p className='text-xl mt-12'>Get in touch <a className='border-b-2 border-b-main hover:bg-blue-800 hover:text-white' href='mailto:phantommy38@gmail.com'>phantommy38@gmail.com</a></p>
+            <p className='text-xl'>Text me on Whatsapp <a className='border-b-2 border-b-main hover:bg-blue-800 hover:text-white'>20+ 01278786671</a></p>
         </div>
         <div className="w-full m-2 flex">
-          <a href="https://github.com/phan-forsure" className="block w-fit mx-2"><img className="w-8 h-8 rounded-xl opacity-50 hover:opacity-85 transition-all cursor-pointer" src="src/assets/githubicon-removebg-preview.png" alt="github" /></a>
-          <a href="https://codepen.io/phan-forsure" className="block w-fit mx-2"><img className="w-8 h-8 rounded-xl opacity-50 hover:opacity-85 transition-all cursor-pointer" src="src/assets/codepen1600.png" alt="codepen" /></a>
+          <a href="https://github.com/phan-forsure" className="block w-fit mx-2"><img className="w-8 h-8 rounded-xl opacity-50 hover:opacity-85 transition-all cursor-pointer" src="githubicon-removebg-preview.png" alt="github" /></a>
+          <a href="https://codepen.io/phan-forsure" className="block w-fit mx-2"><img className="w-8 h-8 rounded-xl opacity-50 hover:opacity-85 transition-all cursor-pointer" src="codepen1600.png" alt="codepen" /></a>
         </div>
     </section>
   )
